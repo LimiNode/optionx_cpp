82cdcfa25df90e86ff3bec59c2e599a93f79348f
Aster Seker <git.electroyar@gmail.com>
2026-08-01 09:51:23 +0300

    fix(telegram): add DTO aggregate header

---
+++ include/optionx_cpp/bridges/telegram.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef OPTIONX_HEADER_BRIDGES_TELEGRAM_HPP_INCLUDED
+#define OPTIONX_HEADER_BRIDGES_TELEGRAM_HPP_INCLUDED
+
+/// \file telegram.hpp
+/// \brief Public Telegram bridge DTO aggregate.
+
+#include "bridges/telegram/TelegramParsedMessage.hpp"
+#include "bridges/telegram/TelegramRawMessage.hpp"
+
+#endif // OPTIONX_HEADER_BRIDGES_TELEGRAM_HPP_INCLUDED
